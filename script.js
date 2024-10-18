802251340e7018523b487cf081374554947afc54
GiovanyN <realgiovanynunez@gmail.com>
2024-10-17 19:03:25 -0700

    #89 & #97 Container displays times of when the prayers occur on current day

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ function showTab(tabName) {
     const activeTab = document.getElementById(tabName);
     activeTab.classList.add('active');
 }
+
+function getDate(){
+    var date = new Date();
+    var day = date.getDate();
+    var dayOfWeek = date.getDate() % 7; // 0 - 23
+    var month = date.getMonth() % 12;
+    var year = date.getFullYear();
+    const dayNames = ["Monday", "Tuesday", "Wednesday", 
+        "Thursday", "Friday", "Saturday", "Sunday"];
+    const monthNames = ["January", "February", "March", 
+        "April", "May", "June", "July", "August", "September",
+        "October", "November", "December"]
+
+    const nth = (day) => {
+        if (day > 3 && day < 21) return 'th';
+        switch (day % 10) {
+            case 1:  return "st";
+            case 2:  return "nd";
+            case 3:  return "rd";
+            default: return "th";
+        }
+    };
+
+
+    document.getElementById("date").innerText = `${dayNames[dayOfWeek]}, ${monthNames[month]} ${day}${nth(day)} ${year}`;
+    
+}
+getDate();
