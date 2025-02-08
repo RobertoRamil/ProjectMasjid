a65399946dfefdbdd77f7e9b7ef476da0285b7fa
Roberto <rramil@csus.edu>
2025-02-07 20:15:44 -0800

    Fixing Merging errors

--- User/header.js
+++ User/header.js
@@ -1,8 +1,5 @@
 // Initialize Firebase
 
-import { fetchLogo, setHeaderBackground } from './firebase.js'
-
-
 window.onload = function() {
   console.log("Window onload event triggered"); // Debugging line
   fetchLogo();
